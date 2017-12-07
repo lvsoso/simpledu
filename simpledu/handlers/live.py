abc5307652060748ac62b2a2def0e44004f630c0
wuqilv <wuqize5109@163.com>
2017-12-07 23:36:41 +0800

    直播页面显示名称和用户名

--- simpledu/handlers/live.py
+++ simpledu/handlers/live.py
@@ -1,9 +1,11 @@
 #coding=utf-8
 
 from flask import Blueprint, render_template
+from simpledu.models import Live
 
 live = Blueprint("live", __name__, url_prefix="/live")
 
 @live.route("/")
 def index():
-    return render_template("live/index.html")
+    live = Live.query.order_by(Live.created_at.desc()).limit(1).first()
+    return render_template("live/index.html", live=live)
